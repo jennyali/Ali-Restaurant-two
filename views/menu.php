a0e7174da9398e6171e3bf863c770b7f70b2135e
Jennifer Ali <Jennifer Ali>
2017-03-27 11:58:47 +0100

    update

---
+++ views/menu.php
@@ -0,0 +1,115 @@
+<?php require './partials/links-header.php'; ?>
+
+<?php require './partials/page-header.php'; ?>
+
+
+<!-- MENU PAGE -->
+
+<main class="container">
+
+    <section class="row">
+
+        <div class="col-sm-12">
+            <h2>Menu</h2>
+        </div>
+    
+    </section>
+
+    <section class="row">
+
+        <div class="col-sm-12">
+
+            <div class="panel panel-default">
+
+                <div class="jumbotron">
+                    <h6>Lunch Menu</h6>
+
+                    <p class="lead">
+                        Get people excited about your menu and your food. Briefly describe it. 
+                    </p>
+                </div>
+
+                <div class="panel-body">
+
+                <h3>SOUPS</h3>
+                <p>This is a section of your menu. Briefly describe it</p>
+
+                <hr>
+
+                <div>
+                    <h5>Chicken Noodle Soup</h5>
+                    <p>This is an item on your menu. Briefly describe it</p>
+                    <span></span>
+                    <p>£9</p>
+                </div>
+
+                <div>
+                    <h5>Chicken Noodle Soup</h5>
+                    <p>This is an item on your menu. Briefly describe it</p>
+                    <span></span>
+                    <p>£9</p>
+                </div>
+
+                <div>
+                    <h5>Chicken Noodle Soup</h5>
+                    <p>This is an item on your menu. Briefly describe it</p>
+                    <span></span>
+                    <p>£9</p>
+                </div>
+
+                <div>
+                    <h5>Chicken Noodle Soup</h5>
+                    <p>This is an item on your menu. Briefly describe it</p>
+                    <span></span>
+                    <p>£9</p>
+                </div>
+
+                <hr>
+
+                <h3>Dumplings</h3>
+                <p>This is a section of your menu. Briefly describe it</p>
+
+                <hr>
+
+                <div>
+                    <h5>Chicken Noodle Soup</h5>
+                    <p>This is an item on your menu. Briefly describe it</p>
+                    <span></span>
+                    <p>£9</p>
+                </div>
+
+                <div>
+                    <h5>Chicken Noodle Soup</h5>
+                    <p>This is an item on your menu. Briefly describe it</p>
+                    <span></span>
+                    <p>£9</p>
+                </div>
+
+                <div>
+                    <h5>Chicken Noodle Soup</h5>
+                    <p>This is an item on your menu. Briefly describe it</p>
+                    <span></span>
+                    <p>£9</p>
+                </div>
+
+                <div>
+                    <h5>Chicken Noodle Soup</h5>
+                    <p>This is an item on your menu. Briefly describe it</p>
+                    <span></span>
+                    <p>£9</p>
+                </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    <section>
+
+</main>
+
+
+<?php require './partials/page-footer.php'; ?>
+
+<?php require './partials/script-footer.php'; ?>
---
+++ views/order.php
@@ -0,0 +1,51 @@
+<?php require './partials/links-header.php'; ?>
+
+<?php require './partials/page-header.php'; ?>
+
+
+<!-- ORDER ONLINE PAGE -->
+
+<main class="container">
+
+    <section class="row">
+
+        <div class="col-sm-12">
+            <h2>Order Online</h2>
+        </div>
+    
+    </section>
+
+    <section class="row well hidden-xs">
+
+        <div class="col-sm-2">
+            <p>Hi, guest</p>
+            <ul>
+                <li>Lunch menu <span class="icon-arrow-66"></span>
+                    <div>
+                        <li> Soups </li>
+                        <li> Dumplings </li>
+                    </div>
+                </li>          
+            </ul>
+        
+        
+        </div>
+
+        <div class="col-sm-7">
+        
+        
+        </div>
+
+        <div class="col-sm-3">
+        
+        
+        </div>
+
+    </section>
+
+</main>
+
+
+<?php require './partials/page-footer.php'; ?>
+
+<?php require './partials/script-footer.php'; ?>
